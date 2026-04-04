b37c21996c4c2e786b5669a86fac006a16db839b
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-04-04 23:20:51 +0530

    Implement isIsomorphic function for string comparison
    Given two strings s and t, determine if they are isomorphic.
    
    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
    
    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.

---
+++ Strings/205. Isomorphic Strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        unordered_map<char, char> m1;
+        unordered_map<char, char> m2;
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            if (m1.count(a) && m1[a] != b) return false;
+            if (m2.count(b) && m2[b] != a) return false;
+
+            m1[a] = b;
+            m2[b] = a;
+        }
+        return true;
+    }
+};
