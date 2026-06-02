0fdd025b62a1fa53a7175a366ba7018a9e4e7f00
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-06-02 18:47:26 +0530

    Implement flood fill algorithm using DFS
    You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
    
    To perform a flood fill:
    
    Begin with the starting pixel and change its color to color.
    Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
    Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
    The process stops when there are no more adjacent pixels of the original color to update.
    Return the modified image after performing the flood fill.

---
+++ Graph/733. Flood Fill.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    private:
+    void dfs(int row,int col,vector<vector<int>>&image,vector<vector<int>> &ans,int color,int iniCol){
+        ans[row][col] = color;
+        int n = image.size();
+        int m = image[0].size();
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+            if(nrow>=0 && nrow < n && ncol >=0 && ncol<m && image[nrow][ncol] == iniCol &&ans[nrow][ncol] != color){
+                dfs(nrow,ncol,image,ans,color,iniCol);
+            }
+        }
+
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>>ans = image;
+        int iniCol = image[sr][sc];
+        dfs(sr,sc,image,ans,color,iniCol);
+        return ans;
+    }
+};
