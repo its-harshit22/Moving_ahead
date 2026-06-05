4f2b2d8672a8ba46d55decbfbbb1468591b1818f
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-06-05 17:25:25 +0530

    Implement cycle detection in 2D grid using DFS

---
+++ Graph/1559. Detect Cycles in 2D Grid.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+    bool dfs(int row, int col,
+             int parentRow, int parentCol,
+             vector<vector<char>>& grid,
+             vector<vector<int>>& visited) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        visited[row][col] = 1;
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        for(int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            if(nrow >= 0 && nrow < n &&
+               ncol >= 0 && ncol < m &&
+               grid[nrow][ncol] == grid[row][col]) {
+
+                if(!visited[nrow][ncol]) {
+                    if(dfs(nrow, ncol, row, col, grid, visited))
+                        return true;
+                }
+                else if(nrow != parentRow || ncol != parentCol) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+public:
+    bool containsCycle(vector<vector<char>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(!visited[i][j]) {
+                    if(dfs(i, j, -1, -1, grid, visited))
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
