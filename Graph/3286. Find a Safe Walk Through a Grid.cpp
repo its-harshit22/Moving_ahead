c27faa6b32f5fd5e08115ee2da5ebf31e535c456
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-07-02 21:30:25 +0530

    Create 3286. Find a Safe Walk Through a Grid.cpp

---
+++ Graph/3286. Find a Safe Walk Through a Grid.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector<vector<int>> best(m, vector<int>(n, -1));
+        queue<pair<pair<int,int>,int>> q;
+
+        int startHealth = health - grid[0][0];
+        if(startHealth <= 0) return false;
+
+        q.push({{0,0}, startHealth});
+        best[0][0] = startHealth;
+
+        int dr[] = {-1,1,0,0};
+        int dc[] = {0,0,-1,1};
+
+        while(!q.empty()){
+            auto cur = q.front();
+            q.pop();
+
+            int r = cur.first.first;
+            int c = cur.first.second;
+            int h = cur.second;
+
+            if(r == m-1 && c == n-1)
+                return true;
+
+            for(int k=0;k<4;k++){
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if(nr<0 || nr>=m || nc<0 || nc>=n)
+                    continue;
+
+                int nh = h - grid[nr][nc];
+
+                if(nh <= 0)
+                    continue;
+
+                if(nh > best[nr][nc]){
+                    best[nr][nc] = nh;
+                    q.push({{nr,nc}, nh});
+                }
+            }
+        }
+
+        return false;
+    }
+};
