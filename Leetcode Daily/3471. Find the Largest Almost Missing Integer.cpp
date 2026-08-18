3f05c82a2b9bb225468dec77b271b3ef0f30ebc4
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-08-18 22:36:20 +0530

    Create 3471. Find the Largest Almost Missing Integer.cpp

---
+++ Leetcode Daily/3471. Find the Largest Almost Missing Integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        map<int, int> mp;
+        int n = nums.size();
+
+        for (int i = 0; i + k <= n; i++) {
+            set<int> st;
+
+            for (int j = i; j < i + k; j++) {
+                st.insert(nums[j]);
+            }
+
+            for (int x : st) {
+                mp[x]++;
+            }
+        }
+
+        int ans = -1;
+
+        for (auto it : mp) {
+            if (it.second == 1) {
+                ans = max(ans, it.first);
+            }
+        }
+
+        return ans;
+    }
+};
