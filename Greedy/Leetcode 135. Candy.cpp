92172b0c9aa5a715dd87b7f5355ad74c606c9ea9
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-08-25 22:52:20 +0530

    Update Leetcode 135. Candy.cpp

--- Greedy/Leetcode 135. Candy.cpp
+++ Greedy/Leetcode 135. Candy.cpp
@@ -30,3 +30,36 @@
 //         return sum;
 //     }
 // };
+class Solution {
+public:
+    int candy(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 1;
+        
+        int i = 1;
+        while(i<n){
+            if(arr[i]==arr[i-1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i<n && arr[i]>arr[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down = 1;
+            while(i<n && arr[i]<arr[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+            if(down > peak){
+                sum = sum + down - peak;
+            }
+        }
+        return sum;
+
+    }
+};
