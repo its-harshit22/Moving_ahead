45cc6013c70b077868cc95e5ec628b867b907cb6
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-09-02 22:49:56 +0530

    Implement longest palindromic subsequence solution

---
+++ Leetcode Daily/Code Testcase Testcase Test Result 516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lcs(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp){
+        if(ind1<0 || ind2<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(s1[ind1]==s2[ind2]){
+            return dp[ind1][ind2] = 1 + lcs(ind1-1,ind2-1,s1,s2,dp);
+        }
+        return dp[ind1][ind2] = max(lcs(ind1-1,ind2,s1,s2,dp),lcs(ind1,ind2-1,s1,s2,dp));
+    }
+    int longestPalindromeSubseq(string s) {
+       string rev(s.rbegin(), s.rend());
+       int n = s.size();
+       vector<vector<int>>dp(n,vector<int>(n,-1));
+       return lcs(n-1,n-1,s,rev,dp);
+    }
+};
