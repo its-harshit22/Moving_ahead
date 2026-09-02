3a54abf60889dc10a230edf0568cd8800f9e8051
Harshit Kushwah <harshitkushwah2203@gmail.com>
2026-09-02 22:57:27 +0530

    Create 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ Dynamic_Programming/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lcs(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp){
+        if(ind1 < 0 || ind2 < 0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(s1[ind1]==s2[ind2]){
+            return dp[ind1][ind2] = 1 + lcs(ind1-1,ind2-1,s1,s2,dp);
+        }
+        return dp[ind1][ind2] = max(lcs(ind1,ind2-1,s1,s2,dp),lcs(ind1-1,ind2,s1,s2,dp));
+    }
+    int minInsertions(string s) {
+        int n = s.size();
+        string s1(s.rbegin(),s.rend());
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return n - lcs(n-1,n-1,s,s1,dp);
+    }
+};
